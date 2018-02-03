13052fa7d42d32821e7dead7cab199020d9f5e82
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-02-03 15:27:15 +0000

    Removed async and await keywords, returned just Task from ValidateUserCredentialsAsync method

--- Authentication.Local/Services/UserService.cs
+++ Authentication.Local/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Authentication.Local.Services
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Models;
@@ -10,12 +11,12 @@
 
         public UserService(IDictionary<string, User> users) => _users = users;
 
-        public async Task<(bool, User)> ValidateUserCredentialsAsync(string username, string password)
+        public Task<(bool, User)> ValidateUserCredentialsAsync(string username, string password)
         {
-            var isValid = await Task.FromResult(_users.ContainsKey(username) &&
-                string.Equals(_users[username].Password, password));
-
-            return (isValid, isValid ? _users[username] : null);
+            var isValid = _users.ContainsKey(username) && 
+                          string.Equals(_users[username].Password, password, StringComparison.Ordinal);
+            var result = (isValid, isValid ? _users[username] : null);
+            return Task.FromResult(result);
         }
     }
 }
