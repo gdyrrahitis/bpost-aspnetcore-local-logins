f7bbe92715e57a2061c0d7043f4041dd42b8dcc9
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-02-24 20:22:03 +0000

    Updated script to insert values for UserClaims

--- Authentication.Local/sql/DML/UserClaims_insert_into.sql
+++ Authentication.Local/sql/DML/UserClaims_insert_into.sql
@@ -2,7 +2,8 @@ begin try
 	begin transaction
 		insert into [dbo].[UserClaims] ([UserId], [Type], [Value], [ValueType])
 		values  (1,'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/gender', 'Male', 'http://www.w3.org/2001/XMLSchema#string'),
-				(1,'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/country', 'USA', 'http://www.w3.org/2001/XMLSchema#string');
+				(1,'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/country', 'USA', 'http://www.w3.org/2001/XMLSchema#string'),
+				(1,'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/mobilephone', '0123456789', 'http://www.w3.org/2001/XMLSchema#string');
 	commit transaction;
 end try
 begin catch
